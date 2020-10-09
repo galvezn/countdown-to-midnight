ea5a94aad8f6ecae37b734bcb58bc9190bcb1c41
galvezn <galvez2902@icloud.com>
2020-10-08 19:20:05 -0500

    Lab Completed

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
-#write your code here
-
 def countdown
+  counter = 10
+  while counter > 0
+    puts "#{counter} Second(s)!"
+    sleep(1)
+    counter -= 1
+  end
+puts "Happy New Year!"
 end
+
+countdown
